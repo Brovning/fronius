e2363eddc9289c558444c57d53019a7a1207d8b6
Brovning <39644775+Brovning@users.noreply.github.com>
2022-02-25 21:48:08 +0100

    #18 Uncaught TypeError: deleteInstanceNotInUse()
    #18 Uncaught TypeError: deleteInstanceNotInUse()

--- libs/myFunctions.php
+++ libs/myFunctions.php
@@ -172,7 +172,7 @@ trait myFunctions
 			// Loesche Connection-Instanz (bspw. ModbusAddress, ClientSocket,...), wenn nicht mehr in Verwendung
 			if (!$inUse)
 			{
-				$returnValue &= IPS_DeleteInstance($connectionId_Old);
+				$returnValue &= (bool)IPS_DeleteInstance($connectionId_Old);
 			}
 		}
 
@@ -483,7 +483,7 @@ trait myFunctions
 		// Zu viele Logwerte in der TimeRange vorhanden
 		elseif (10000 <= count($buffer))
 		{
-			$intervallEdge = $startTime + ($endTime - $startTime) / 2;
+			$intervallEdge = (int)($startTime + ($endTime - $startTime) / 2);
 			$bufferSum = $this->getPowerSumOfLog($logId, $startTime, $intervallEdge, $mode) + $this->getPowerSumOfLog($logId, $intervallEdge, $endTime, $mode);
 		}
 		// Logwerte vorhanden
